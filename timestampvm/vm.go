56ff50651667acdcb110233baf9c1e86fd325f62
Ceyhun Onur <ceyhun.onur@avalabs.org>
2022-03-12 22:23:38 +0300

    bump timstampvm version to 1.2.3

--- timestampvm/vm.go
+++ timestampvm/vm.go
@@ -31,7 +31,7 @@ const (
 var (
 	errNoPendingBlocks = errors.New("there is no block to propose")
 	errBadGenesisBytes = errors.New("genesis data should be bytes (max length 32)")
-	Version            = version.NewDefaultVersion(1, 2, 2)
+	Version            = version.NewDefaultVersion(1, 2, 3)
 
 	_ block.ChainVM = &VM{}
 )
