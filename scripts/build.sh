dc93d9ccddacad8b8c7c7502bd5a2b3c2bdef11a
Patrick O'Grady <prohb125@gmail.com>
2022-11-14 17:37:24 -0800

    fix unit tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+
 # Load the constants
 # Set the PATHS
 GOPATH="$(go env GOPATH)"
@@ -27,7 +33,6 @@ elif [[ $# -ne 0 ]]; then
     echo "Invalid arguments to build timestampvm. Requires either no arguments (default) or one arguments to specify binary location."
     exit 1
 fi
-
 
 # Build timestampvm, which is run as a subprocess
 echo "Building timestampvm in $binary_directory/$name"
