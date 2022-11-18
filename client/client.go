93697494aa767253fa45f9395536c5b23aa08b23
Patrick O'Grady <prohb125@gmail.com>
2022-11-18 08:23:38 -0800

    update license

--- client/client.go
+++ client/client.go
@@ -1,5 +1,5 @@
-// // (c) 2019-2022, Ava Labs, Inc. All rights reserved.
-// // See the file LICENSE for licensing terms.
+// (c) 2019-2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
 
 package client
 
