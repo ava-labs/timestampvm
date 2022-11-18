a38af68278486271caa50f983121e555a28ee3c2
Patrick O'Grady <prohb125@gmail.com>
2022-11-18 08:15:47 -0800

    update comments

--- scripts/tests.lint.sh
+++ scripts/tests.lint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# // (c) 2019-2022, Ava Labs, Inc. All rights reserved.
-# // See the file LICENSE for licensing terms.
+# (c) 2019-2022, Ava Labs, Inc. All rights reserved.
+# See the file LICENSE for licensing terms.
 
 set -o errexit
 set -o pipefail
